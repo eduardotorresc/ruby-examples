eeb641155438ff89cd628ca788f1ffc4d91f4e94
Eduardo Torres C <eduardotorresc7@gmail.com>
2019-12-13 15:04:46 -0500

    add examples.rb

---
+++ examples.rb
@@ -0,0 +1,27 @@
+phrase = "Wellcome"
+number1 = 4
+number2 = -3
+number3 = 4.7
+number4 = 4.1
+number5 = 4.9
+
+puts phrase[1, 3]
+puts phrase.upcase()
+puts phrase.downcase()
+puts phrase.index("W")
+puts phrase.index("come")
+
+puts number1 + 3
+puts number1 - 3
+puts number1 * 3
+puts number1 / 3
+puts number1 % 3
+puts number1 ** 3
+
+puts ("My number is " + number1.to_s)
+
+puts number2.abs()
+puts number3.round()
+puts number4.ceil()
+puts number4.floor()
+puts Math.sqrt(64)
